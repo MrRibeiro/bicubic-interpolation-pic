d5883fe6670d5b177b816d5bebfcba9b059693df
david <david>
2019-05-29 17:56:37 -0300

    init

---
+++ bicubic4pic.X/build/default/production/newmain.p1.d
@@ -0,0 +1,13 @@
+ build/default/production/newmain.d  \
+ build/default/production/newmain.p1:  \
+ newmain.c  \
+../../../../../../Program\ Files\ (x86)/Microchip/xc8/v2.00/pic/include/c99/musl_xc8.h  \
+../../../../../../Program\ Files\ (x86)/Microchip/xc8/v2.00/pic/include/c99/stdio.h  \
+../../../../../../Program\ Files\ (x86)/Microchip/xc8/v2.00/pic/include/c99/bits/alltypes.h  \
+../../../../../../Program\ Files\ (x86)/Microchip/xc8/v2.00/pic/include/c99/string.h  \
+../../../../../../Program\ Files\ (x86)/Microchip/xc8/v2.00/pic/include/c99/features.h  \
+../../../../../../Program\ Files\ (x86)/Microchip/xc8/v2.00/pic/include/c99/math.h  \
+../../../../../../Program\ Files\ (x86)/Microchip/xc8/v2.00/pic/include/c99/bits/stdint.h  \
+../../../../../../Program\ Files\ (x86)/Microchip/xc8/v2.00/pic/include/c99/stdint.h  \
+../../../../../../Program\ Files\ (x86)/Microchip/xc8/v2.00/pic/include/c99/stdlib.h  \
+../../../../../../Program\ Files\ (x86)/Microchip/xc8/v2.00/pic/include/c99/time.h 
